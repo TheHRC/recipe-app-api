445a7221efce4cfff56b692ee4635429eef9a0b0
Hashib <hashibur.rahman.chowdhury@gmail.com>
2019-08-05 19:36:02 +0530

    Fix W391 blank line at end of file in test/test_models.py

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import (
-            AbstractBaseUser,
-            BaseUserManager,
-            PermissionsMixin
+            AbstractBaseUser, BaseUserManager, PermissionsMixin
         )
 
 
